d233dd0f5ccd90aefd818de4cad7693fb172e668
billionaire <me@billionaire.name>
2024-08-19 22:03:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import json
+import os
 import pickle
 import pandas as pd
 import numpy as np
 from datetime import datetime
 from flask import Flask, jsonify, Response
 from model import download_data, format_data, train_model
-from config import model_file_path
+from config import model_file_path, data_base_path
 
 app = Flask(__name__)
 
@@ -16,10 +17,13 @@ def update_data():
     format_data()
     train_model()
 
+def get_inference(token):
+    """Load model and predict current price based on token."""
+    if token not in ["ETH", "SOL", "BTC", "ARB", "BNB"]:
+        raise ValueError("Token not supported")
 
-def get_eth_inference():
-    """Load model and predict current price."""
-    with open(model_file_path, "rb") as f:
+    model_file_path_token = os.path.join(data_base_path, f"model_{token}USDT.pkl")
+    with open(model_file_path_token, "rb") as f:
         loaded_model = pickle.load(f)
 
     now_timestamp = pd.Timestamp(datetime.now()).timestamp()
@@ -28,20 +32,20 @@ def get_eth_inference():
 
     return current_price_pred[0][0]
 
-
 @app.route("/inference/<string:token>")
 def generate_inference(token):
     """Generate inference for given token."""
-    if not token or token != "ETH":
-        error_msg = "Token is required" if not token else "Token not supported"
+    if not token:
+        error_msg = "Token is required"
         return Response(json.dumps({"error": error_msg}), status=400, mimetype='application/json')
 
     try:
-        inference = get_eth_inference()
+        inference = get_inference(token)
         return Response(str(inference), status=200)
+    except ValueError as e:
+        return Response(json.dumps({"error": str(e)}), status=400, mimetype='application/json')
     except Exception as e:
         return Response(json.dumps({"error": str(e)}), status=500, mimetype='application/json')
-
 
 @app.route("/update")
 def update():
