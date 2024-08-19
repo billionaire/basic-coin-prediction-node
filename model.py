c5c23c6c7b02417544595b99350d070c9cf49c33
billionaire <me@billionaire.name>
2024-08-19 21:56:23 +0200

    Update model.py

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ training_price_data_path = os.path.join(data_base_path, "eth_price_data.csv")
 
 def download_data():
     cm_or_um = "um"
-    symbols = ["ETHUSDT"]
+    symbols = ["ETHUSDT", "SOLUSDT", "BTCUSDT", "ARBUSDT", "BNBUSDT"]
     intervals = ["1d"]
     years = ["2020", "2021", "2022", "2023", "2024"]
     months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
@@ -32,42 +32,39 @@ def download_data():
     )
     print(f"Downloaded daily data to {download_path}.")
 
-
 def format_data():
-    files = sorted([x for x in os.listdir(binance_data_path)])
-
-    # No files to process
-    if len(files) == 0:
-        return
-
     price_df = pd.DataFrame()
-    for file in files:
-        zip_file_path = os.path.join(binance_data_path, file)
+    for symbol in ["ETHUSDT", "SOLUSDT", "BTCUSDT", "ARBUSDT", "BNBUSDT"]:
+        files = sorted([x for x in os.listdir(binance_data_path) if symbol in x])
 
-        if not zip_file_path.endswith(".zip"):
-            continue
+        for file in files:
+            zip_file_path = os.path.join(binance_data_path, file)
 
-        myzip = ZipFile(zip_file_path)
-        with myzip.open(myzip.filelist[0]) as f:
-            line = f.readline()
-            header = 0 if line.decode("utf-8").startswith("open_time") else None
-        df = pd.read_csv(myzip.open(myzip.filelist[0]), header=header).iloc[:, :11]
-        df.columns = [
-            "start_time",
-            "open",
-            "high",
-            "low",
-            "close",
-            "volume",
-            "end_time",
-            "volume_usd",
-            "n_trades",
-            "taker_volume",
-            "taker_volume_usd",
-        ]
-        df.index = [pd.Timestamp(x + 1, unit="ms") for x in df["end_time"]]
-        df.index.name = "date"
-        price_df = pd.concat([price_df, df])
+            if not zip_file_path.endswith(".zip"):
+                continue
+
+            myzip = ZipFile(zip_file_path)
+            with myzip.open(myzip.filelist[0]) as f:
+                line = f.readline()
+                header = 0 if line.decode("utf-8").startswith("open_time") else None
+            df = pd.read_csv(myzip.open(myzip.filelist[0]), header=header).iloc[:, :11]
+            df.columns = [
+                "start_time",
+                "open",
+                "high",
+                "low",
+                "close",
+                "volume",
+                "end_time",
+                "volume_usd",
+                "n_trades",
+                "taker_volume",
+                "taker_volume_usd",
+            ]
+            df['symbol'] = symbol
+            df.index = [pd.Timestamp(x + 1, unit="ms") for x in df["end_time"]]
+            df.index.name = "date"
+            price_df = pd.concat([price_df, df])
 
     price_df.sort_index().to_csv(training_price_data_path)
 
@@ -75,30 +72,28 @@ def format_data():
 def train_model():
     # Load the eth price data
     price_data = pd.read_csv(training_price_data_path)
-    df = pd.DataFrame()
+    unique_symbols = price_data['symbol'].unique()
 
-    # Convert 'date' to a numerical value (timestamp) we can use for regression
-    df["date"] = pd.to_datetime(price_data["date"])
-    df["date"] = df["date"].map(pd.Timestamp.timestamp)
+    for symbol in unique_symbols:
+        df = price_data[price_data['symbol'] == symbol].copy()
+        df["date"] = pd.to_datetime(df["end_time"], unit='ms')
+        df["date"] = df["date"].map(pd.Timestamp.timestamp)
+        df["price"] = df[["open", "close", "high", "low"]].mean(axis=1)
 
-    df["price"] = price_data[["open", "close", "high", "low"]].mean(axis=1)
+        # Reshape the data to the shape expected by sklearn
+        x = df["date"].values.reshape(-1, 1)
+        y = df["price"].values.reshape(-1, 1)
 
-    # Reshape the data to the shape expected by sklearn
-    x = df["date"].values.reshape(-1, 1)
-    y = df["price"].values.reshape(-1, 1)
+        # Split the data into training set and test set
+        x_train, _, y_train, _ = train_test_split(x, y, test_size=0.2, random_state=0)
 
-    # Split the data into training set and test set
-    x_train, _, y_train, _ = train_test_split(x, y, test_size=0.2, random_state=0)
+        # Train the model
+        model = LinearRegression()
+        model.fit(x_train, y_train)
 
-    # Train the model
-    model = LinearRegression()
-    model.fit(x_train, y_train)
+        # Save the trained model to a file (considering different model files for each token)
+        model_file_path_token = os.path.join(data_base_path, f"model_{symbol}.pkl")
+        with open(model_file_path_token, "wb") as f:
+            pickle.dump(model, f)
 
-    # create the model's parent directory if it doesn't exist
-    os.makedirs(os.path.dirname(model_file_path), exist_ok=True)
-
-    # Save the trained model to a file
-    with open(model_file_path, "wb") as f:
-        pickle.dump(model, f)
-
-    print(f"Trained model saved to {model_file_path}")
+        print(f"Trained model for {symbol} saved to {model_file_path_token}")
